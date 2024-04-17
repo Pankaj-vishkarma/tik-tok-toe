d5e61d7ac4df81eba29427c83c62bd1a4b369905
pankaj vishwakarma <pankaj19jnp@gmail.com>
2024-04-17 16:07:06 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Grid from './componets/Grid'
+
+
+
+
+
+function App() {
+  return (
+    <>
+      <Grid numberOfCards={9}/>
+    </>
+  )
+}
+
+export default App
